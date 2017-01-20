1aea90b944f18593c67f7b69e9adb24efd2fad3f
ernir <e.ernir@gmail.com>
2017-01-20 01:06:37 +0000

    Bæta umfjöllun um hlutbundna forritun við glærupakka 2

---
+++ T2/Code/w2/constructhuman.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Human {
+private:
+    string name;
+    int age;
+public:
+    Human(string humansName, int humansAge) {
+        this->name = humansName;
+        this->age = humansAge;
+        cout << "Overloaded constructor creates ";
+        cout << this->name;
+        cout << " of age " << this->age << endl;
+    }
+
+    void IntroduceSelf() {
+        cout << "I am " + name << " and am ";
+        cout << age << " years old" << endl;
+    }
+};
+
+int main() {
+    Human firstMan("Adam", 25);
+    Human firstWoman("Eve", 28);
+    firstMan.IntroduceSelf();
+    firstWoman.IntroduceSelf();
+}
---
+++ T2/Code/w2/fishies.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+
+using namespace std;
+
+class Fish {
+public:
+    bool isFreshWaterFish;
+
+    void swim() {
+        if (isFreshWaterFish)
+            cout << "Swims in lake" << endl;
+        else
+            cout << "Swims in sea" << endl;
+    }
+};
+
+class Tuna : public Fish {
+public:
+    Tuna() {
+        isFreshWaterFish = false;
+    }
+};
+
+class Carp : public Fish {
+public:
+    Carp() {
+        isFreshWaterFish = true;
+    }
+};
+
+int main() {
+    Carp myLunch;
+    Tuna myDinner;
+    cout << "About my food:" << endl;
+    cout << "Lunch: ";
+    myLunch.swim();
+    cout << "Dinner: ";
+    myDinner.swim();
+    return 0;
+}
