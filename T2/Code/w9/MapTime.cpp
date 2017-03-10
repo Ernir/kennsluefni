51de86a5053bea760d326c292a61a9b253f721d1
ernir <e.ernir@gmail.com>
2017-03-10 00:07:10 +0000

    Klára skilaverkefni 9
    Bætt við isBalanced() dæmi, endurröðun dæma

---
+++ T2/Code/w9/MapTime.cpp
@@ -0,0 +1,32 @@
+#include <map>
+#include <unordered_map>
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    vector<string> words;
+    vector<string> wordsShuffled;
+
+    /*
+     * Hér þarf að gefa vigrunum gildi.
+     * Ef nota á fleiri vigra væri viðeigandi að gefa þeim gildi hér líka.
+     */
+
+    // Gefið dæmi: Tímamæling á innsetningu í map
+    map<string, int> myMap;
+    clock_t c;
+    c = clock();
+    for (int i = 0; i < wordsShuffled.size(); i++) {
+        myMap.insert(pair<string, int>(wordsShuffled[i], i));
+    }
+    cout << "Innsetningu strengja í map lokið á: " << (clock() - c) << " klukkupúlsum" << endl;
+
+    /*
+     * Hér þarf að framkvæma hinar prófanirnar sjö
+     */
+    return 0;
+}
