1a45da763d45d50c5bae0e0c53865ea71cd35a87
Ernir <e.ernir@gmail.com>
2018-03-14 11:15:45 +0000

    Uppfæra skilaverkefni 9 fyrir 2018.
    Verkefnið er fyrst og fremst stytt frá fyrra ári.
    Ónýtt dæmi um að skrifa aðgerðir fyrir tvíleitartré sem þegar voru til staðar í algs4.jar ver fjarlægt.
    Tímamælingadæmið er stytt og betri ráðgjöf gefin í síðasta dæminu.

---
+++ T2/Code/w10/fibotime.cpp
@@ -0,0 +1,40 @@
+#include <algorithm>
+#include <iostream>
+#include <map>
+#include <string>
+#include <unordered_map>
+#include <vector>
+
+/*
+ * ATH: Á sumum vélum þarf að afkommenta aðra eða báðar af eftirfarandi 
+ * línum til að forritið þýðist umkvörtunarlaust.
+ */
+// #include <time.h>
+// extern clock_t clock(void);
+
+using namespace std;
+
+int fibo(int n) {
+    /*
+     * Vond útfærsla á útreikningi á n-tu Fibonacci tölunni.
+     * Valin til að við sjáum örlítið langan keyrslutíma.
+     */
+    if (n < 1) {
+        return 0;
+    } else if (n == 1) {
+        return 1;
+    } else {
+        return fibo(n - 1) + fibo(n - 2);
+    }
+}
+
+int main() {
+    // Tímamælum útreikning á 20. Fibonacci tölunni
+    int n = 30;
+    clock_t c1,c2;
+    c1 = clock();
+    fibo(n);
+    c2 = clock();
+    cout << "Útreikningi á fib(" << n << ") lokið á " << (c2 - c1) << " klukkupúlsum" << endl;
+    return 0;
+}
