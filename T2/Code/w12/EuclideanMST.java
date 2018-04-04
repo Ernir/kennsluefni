df50dbaf0bad077bb7f8db15ac79ff925866ddc8
Ernir <e.ernir@gmail.com>
2018-04-04 23:27:42 +0000

    Uppfæra skilaverkefni 11 fyrir 2018
    Breyta Katar-verkefninu í Lúxemborgarverkefni.
    Henda út völundarhúsinu, setja Schulze í staðinn.

---
+++ T2/Code/w12/EuclideanMST.java
@@ -0,0 +1,102 @@
+import edu.princeton.cs.algs4.*;
+
+public class EuclideanMST {
+
+    // Fastar sem skilgreina mörk kortsins sem á að teikna.
+    private double xMin = Double.POSITIVE_INFINITY;
+    private double xMax = Double.NEGATIVE_INFINITY;
+    private double yMin = Double.POSITIVE_INFINITY;
+    private double yMax = Double.NEGATIVE_INFINITY;
+
+    private Point2D[] points; // Skilgreinið þessa breytu í smiðnum.
+    private KruskalMST mst; // Skilgreinið þessa breytu í smiðnum. Hér mætti líka nota PrimMST.
+
+    public EuclideanMST(In in) {
+        /*
+         * Les hnit úr inntaksstraumnum <in> og upphafsstillir tilviksbreyturnar <this.mst> og <this.points>.
+         */
+
+        // Hér þarf að skrifa kóða!
+        // Sterklega er mælt með því að skrifa hjálparföll.
+
+        // Hugmyndir að hjálparföllum:
+        // 1. Fall til að breyta línunum í luxembourg.txt í fylki af Point2D
+        // 2. Fall til að búa til fullskipað EdgeWeightedGraph út frá <this.points>, 
+        // þar sem þyngd leggjanna er fjarlægðin á milli punktanna.
+    }
+
+    private void drawMST() {
+        /*
+         * Teiknar spanntréð í tilviksbreytunni <this.mst> með hnitin <this.points>.
+         * Gerir ráð fyrir að <this.xMax>, <this.xMin>, <this.yMax> og <this.yMin> séu upphafsstillt.
+         */
+        // Hér þarf að skrifa kóða!
+    }
+
+    public void drawPoints() {
+        /*
+         * Teiknar hnitin í <this.points> á striga.
+         * Gerir ráð fyrir að <this.xMin> og <this.yMin> hafi verið upphafsstillt. Sjá initializeCanvas().
+         *
+         */
+        for (Point2D p : points) {
+            StdDraw.point(p.x() - xMin, p.y() - yMin);
+        }
+    }
+
+    public void initializeCanvas() {
+        /*
+         * Upphafsstillir striga út frá gögnunum í <this.points>.
+         * Setur vitræn gildi á <this.xMax>, <this.xMin>, <this.yMax> og <this.yMin>.
+         */
+
+        // Útgildin fundin
+        for (Point2D point : points) {
+            if (point.x() < xMin) {
+                xMin = point.x();
+            }
+            if (xMax < point.x()) {
+                xMax = point.x();
+            }
+            if (point.y() < yMin) {
+                yMin = point.y();
+            }
+            if (yMax < point.y()) {
+                yMax = point.y();
+            }
+        }
+        // Ramminn stækkaður aðeins svo allir punktar geti sést
+        double xDiff = xMax - xMin;
+        double yDiff = yMax - yMin;
+        double padding = 0.1; // Stillir hlutfallslega stærð hvíta rýmisins í kringum punktana
+        xMin = xMin - xDiff * padding / 2;
+        xMax = xMax + xDiff * padding / 2;
+        yMin = yMin - yDiff * padding / 2;
+        yMax = yMax + yDiff * padding / 2;
+
+        // Striginn sjálfur upphafsstilltur
+        StdDraw.setCanvasSize((int) (xMax - xMin), (int) (yMax - yMin));
+        StdDraw.setXscale(0, (int) (xMax - xMin));
+        StdDraw.setYscale(0, (int) (yMax - yMin));
+        StdDraw.setPenRadius(0.01);
+    }
+
+    public double getWeight() {
+        /*
+        Sækir þyngd undirliggjandi spanntrés
+        */
+        return this.mst.weight();
+    }
+
+    public static void main(String[] args) {
+        // ATH: Þetta hrynur við keyrslu þar til points og mst tilviksbreyturnar hafa verið skilgreindar.
+        EuclideanMST euclideanMST = new EuclideanMST(new In("luxembourg.txt"));
+
+        StdOut.println(euclideanMST.getWeight());
+
+        euclideanMST.initializeCanvas();
+        euclideanMST.drawPoints();
+        euclideanMST.drawMST();
+    }
+
+}
---
+++ T2/Code/w12/Schulze.java
@@ -0,0 +1,46 @@
+public class Schulze {
+
+    public static int[][] floydWarshall(int[][] d, int n) {
+        /*
+        Útgáfa af Floyd-Warshall reikniritinu til nota í Schulze kosningakerfinu.
+
+        Tekur inn tvívítt fylki d, þar sem d[i][j] er fjöldi kjósenda sem kýs
+        frambjóðanda i fram yfir frambjóðanda j, ásamt fjölda frambjóðenda n.
+
+        Skilar tvívíðu fylki p, þar sem p[i][j] er styrkur sterkasta vegs frá i til j.
+         */
+        int[][] p = new int[n][n];
+        for (int i = 0; i < n; i++) { // Upphafsstilling fylkisins p
+            for (int j = 0; j < n; j++) {
+                if (i != j) {
+                    if (d[i][j] > d[j][i]) {
+                        p[i][j] = d[i][j];
+                    } else {
+                        p[i][j] = 0;
+                    }
+                }
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i != j) {
+                    for (int k = 0; k < n; k++) {
+                        if (i != k && j != k) {
+                            p[j][k] = Math.max(p[j][k], Math.min(p[j][i], p[i][k])); // "veikasti hlekkurinn" uppfærður
+                        }
+                    }
+                }
+            }
+        }
+        return p;
+    }
+
+
+    public static void main(String[] args) {
+        String[] votes = new In("votes.txt").readAllLines();
+        int numCandidates = Integer.parseInt(votes[0]);
+
+        // Hér þarf að skrifa kóða!
+        // Mælt er með því að skrifa hjálparaðferðir.
+    }
+}
